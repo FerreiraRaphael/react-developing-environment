610fa58b59c5c7ee2e958bb630301950b0a19ee1
raphael ferreira <raphael.ferreira@workcard.com.br>
2017-05-29 18:05:04 -0300

    fix on bug at generateMockData

--- tools/generateMockData.js
+++ tools/generateMockData.js
@@ -4,7 +4,7 @@
 import jsf from 'json-schema-faker';
 import fs from 'fs';
 import chalk from 'chalk';
-import { schema } from '../spec/mockDataSchema';
+import schema from '../spec/mockDataSchema';
 
 const json = JSON.stringify(jsf(schema), null, 2);
 fs.writeFile('./src/api/db.json', json, err =>
