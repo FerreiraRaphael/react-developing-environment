0d9cd44eca9ce20c93ce735b550f77c44ba8992c
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-06-02 07:40:45 -0300

    fix hello component

--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 
-const Hello = props => <h1>Hello afffffffffffeeeeeeeeeeeeeeeeeee {props.name}</h1>;
+const Hello = props => <h1>Hello {props.name}</h1>;
 
 Hello.propTypes = {
   name: Proptypes.string.isRequired,
